20c57a96c0852dc6139f120b5d34d4b82ca3fe51
otetLopez <otet.tud@gmail.com>
2019-11-10 19:16:58 -0500

    Avant 3115 Project: Adding initial Error Code file

---
+++ Avant/Avant/src/ErrorCodes.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorCodes.swift
+//  Avant
+//
+//  Created by otet_tud on 11/10/19.
+//  Copyright © 2019 otet_tud. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorCodes {
+    case SUCESSFUL
+    case ERROR_UKNOWN
+}
