faab3c78f4b245ce30c9af216f61d30afa45f2ff
otetLopez <otet.tud@gmail.com>
2019-11-15 15:34:05 -0500

    Adding Date Picker

--- Avant/Avant/ComposeTableViewController.swift
+++ Avant/Avant/ComposeTableViewController.swift
@@ -12,9 +12,17 @@ class ComposeTableViewController: UITableViewController {
 
     var datePickerIndexPath: IndexPath?
     var isPickingDate : Bool = false
+    var inputTexts: [String] = ["Start date", "End date", "Another date"]
+    var inputDates: [Date] = []
     //@IBOutlet weak var planeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addInitailValues()
+        
+        tableView.register(UINib(nibName: DateTableViewCell.nibName(), bundle: nil), forCellReuseIdentifier: DateTableViewCell.reuseIdentifier())
+        tableView.register(UINib(nibName: DatePickerTableViewCell.nibName(), bundle: nil), forCellReuseIdentifier: DatePickerTableViewCell.reuseIdentifier())
+        //tableView.register(DatePickerTableViewCell.self, forCellReuseIdentifier: "compose")
         //planeButton.tintColor = UIColor.darkGray
 
         // Uncomment the following line to preserve selection between presentations
@@ -33,7 +41,6 @@ class ComposeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        print("DEBUG: Changing number of rows")
         return  section == 0 ? (datePickerIndexPath != nil ? 5 : 4) : 1
     
     }
@@ -41,12 +48,12 @@ class ComposeTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
         if indexPath.row == 3{
-            print("DEBUG: Setting Schedule \(indexPath.row + 1)")
             tableView.beginUpdates()
             if let datePickerIndexPath = datePickerIndexPath, datePickerIndexPath.row - 1 == indexPath.row {
                 tableView.deleteRows(at: [datePickerIndexPath], with: .fade)
                 self.datePickerIndexPath = nil
             } else {
+                print("DEBUG: Setting DatePicker")
                 if let datePickerIndexPath = datePickerIndexPath {
                     tableView.deleteRows(at: [datePickerIndexPath], with: .fade)
                 }
@@ -58,7 +65,12 @@ class ComposeTableViewController: UITableViewController {
         }
     }
     
+    func addInitailValues() {
+        inputDates = Array(repeating: Date(), count: 10)
+    }
+    
     func indexPathToInsertDatePicker(indexPath: IndexPath) -> IndexPath {
+        print("DEBUG: Inserting Date Picker")
         if let datePickerIndexPath = datePickerIndexPath, datePickerIndexPath.row < indexPath.row {
             return indexPath
         } else {
@@ -66,39 +78,66 @@ class ComposeTableViewController: UITableViewController {
         }
     }
     
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "compose", for: indexPath)
-        // Configure the cell...
-        let date = Date()
-        let formatter = DateFormatter()
-        formatter.dateFormat = "dd-MM-yy HH:MM"
-        let result = formatter.string(from: date)
-        
-        var cellLbl : String = ""
-        if indexPath.section == 0 {
-            switch indexPath.row {
-            case 0:
-                cellLbl = "To:"
-            case 1:
-                cellLbl = "From:"
-            case 2:
-                cellLbl = "Subject"
-            case 3:
-                cellLbl = "Schedule"
-                print("DEBUG: The date today is \(result)")
-                cell.detailTextLabel?.text = result
-            default: break
-            }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if datePickerIndexPath == indexPath {
+            return DatePickerTableViewCell.cellHeight()
+        } else {
+            return DateTableViewCell.cellHeight()
         }
-        else if indexPath.section == 1 {
-                //cell.textInputContextIdentifier = "You Message"
-        }
-        cell.textLabel?.text = cellLbl
-        return cell
     }
     
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        if datePickerIndexPath == indexPath {
+            let datePickerCell = tableView.dequeueReusableCell(withIdentifier: DatePickerTableViewCell.reuseIdentifier()) as! DatePickerTableViewCell
+            datePickerCell.updateCell(date: inputDates[indexPath.row - 1], indexPath: indexPath)
+            //datePickerCell.delegate = self
+            return datePickerCell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "compose", for: indexPath)
+            // Configure the cell...
+            let date = Date()
+            let formatter = DateFormatter()
+            formatter.dateFormat = "dd-MM-yy HH:MM"
+            let result = formatter.string(from: date)
+            
+            var cellLbl : String = ""
+            if indexPath.section == 0 {
+                switch indexPath.row {
+                case 0:
+                    cellLbl = "To:"
+                case 1:
+                    cellLbl = "From:"
+                case 2:
+                    cellLbl = "Subject"
+                case 3:
+                    cellLbl = "Schedule"
+                    print("DEBUG: The date today is \(result)")
+                    cell.detailTextLabel?.text = result
+                default: break
+                }
+            }
+            else if indexPath.section == 1 {
+                    //cell.textInputContextIdentifier = "You Message"
+            }
+            cell.textLabel?.text = cellLbl
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+   // extension ComposeTableViewController: DatePickerDelegate {
+        
+        func didChangeDate(date: Date, indexPath: IndexPath) {
+            inputDates[indexPath.row] = date
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+  // }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
